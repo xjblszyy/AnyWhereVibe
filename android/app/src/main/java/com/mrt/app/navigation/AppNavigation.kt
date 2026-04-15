bc24d2fcdd1f4c30916228bffb9e3d9e31964cca
Song Ziyangyi <songzyy@chinatelecom.cn>
2026-04-15 11:15:04 +0800

    fix: wire android local sdk

--- android/app/src/main/java/com/mrt/app/navigation/AppNavigation.kt
+++ android/app/src/main/java/com/mrt/app/navigation/AppNavigation.kt
@@ -22,8 +22,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
